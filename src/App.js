cf47f5feb89e046ff980bf28bad89c935c791f99
Rio Nugroho <rio.nugrosz@gmail.com>
2019-01-10 15:19:34 +0700

    change input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Label from './component/Label';
 import Input from './component/Input';
 import Button from './component/Button';
+import List from './component/List';
 import axios from 'axios';
 
 class App extends Component {
@@ -13,9 +14,12 @@ class App extends Component {
       inputTo: '',
       resultConvert: '',
       ajaxResult: '',
+      country: []
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.inputClick = this.inputClick.bind(this);
+    this.listClick = this.listClick.bind(this);
   }
 
   handleChange({target}) {
@@ -29,18 +33,40 @@ class App extends Component {
     .then(res => {
       const ajaxResult = res.data;
       let rates = '';
-      this.setState({ ajaxResult });
       for(rates in ajaxResult.rates) {
-        this.setState({resultConvert:ajaxResult.rates[rates] * inputAmount});
+        this.setState({
+          resultConvert: ajaxResult.rates[rates] * inputAmount
+        });
       }
     });
   }
 
   handleClick() {
     const inputAmount = Number(this.state.inputAmount);
-    const inputFrom = this.state.inputFrom;
-    const inputTo = this.state.inputTo;
+    const inputFrom = this.state.inputFrom.toUpperCase();
+    const inputTo = this.state.inputTo.toUpperCase();
     this.startConvert(inputAmount, inputFrom, inputTo);
+  }
+
+  componentDidMount(){
+    axios.get(`https://api.exchangeratesapi.io/latest?`)
+    .then(res => {
+      const country = res.data;
+      let rates = '';
+      for(rates in country.rates) {
+        this.setState({country: this.state.country.concat(rates)});
+      }
+    });
+  }
+
+  inputClick() {
+    
+  }
+
+  listClick(e) {
+    e.target.dataset.value = this.state.inputFrom;
+    e.target.elements.inputFrom.value = e.target.dataset.value;
+    console.log(this.state.inputFrom);
   }
 
   render() {
@@ -49,11 +75,12 @@ class App extends Component {
         <div className="wrap-input clearfix">
           <div className="box">
             <Label className="label" label={'Amount'}/>
-            <Input className="input" type="text" name={'inputAmount'} value={this.state.inputAmount} onChange={this.handleChange}/>
+            <Input className="input" type="number" name={'inputAmount'} value={this.state.inputAmount} onChange={this.handleChange}/>
           </div>
           <div className="box">
             <Label className="label" label={'From'}/>
-            <Input className="input" type="text" name={'inputFrom'} value={this.state.inputFrom} onChange={this.handleChange}/>
+            <Input className="input" type="text" name={'inputFrom'} value={this.state.inputFrom} onChange={this.handleChange} onClick={this.inputClick}/>
+            <List className="list-country" country={this.state.country} onClick={this.listClick}/>
           </div>
           <div className="box">
             <Label className="label" label={'To'}/>
--- src/component/Input.js
+++ src/component/Input.js
@@ -2,29 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({
+    className,
     type,
     name,
     value,
-    onChange,
-    className
+    onChange
 }) => {
     return (
         <input
+            className={className}
             type={type}
             name={name}
             value={value}
             onChange={onChange}
-            className={className}
         />
     )
 }
 
 Input.propType = {
+    list: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    className: PropTypes.string.isRequired
+    onChange: PropTypes.func.isRequired
 }
 
 export default Input;
