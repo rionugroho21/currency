3f674e7f24c59595414ef335ff17562a7f800201
Rio Nugroho <rio.nugrosz@gmail.com>
2019-01-10 16:06:58 +0700

    add note

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.inputClick = this.inputClick.bind(this);
-    this.listClick = this.listClick.bind(this);
+    this.listClick1 = this.listClick1.bind(this);
+    this.listClick2 = this.listClick2.bind(this);
   }
 
   handleChange({target}) {
@@ -63,10 +64,15 @@ class App extends Component {
     
   }
 
-  listClick(e) {
-    e.target.dataset.value = this.state.inputFrom;
-    e.target.elements.inputFrom.value = e.target.dataset.value;
-    console.log(this.state.inputFrom);
+  listClick1(e) {
+    this.setState({
+      inputFrom: e.target.dataset.value
+    });
+  }
+  listClick2(e) {
+    this.setState({
+      inputTo: e.target.dataset.value
+    });
   }
 
   render() {
@@ -80,17 +86,19 @@ class App extends Component {
           <div className="box">
             <Label className="label" label={'From'}/>
             <Input className="input" type="text" name={'inputFrom'} value={this.state.inputFrom} onChange={this.handleChange} onClick={this.inputClick}/>
-            <List className="list-country" country={this.state.country} onClick={this.listClick}/>
+            {/* <List className="list-country" country={this.state.country} onClick={this.listClick1}/> */}
           </div>
           <div className="box">
             <Label className="label" label={'To'}/>
-            <Input className="input" type="text" name={'inputTo'} value={this.state.inputTo} onChange={this.handleChange}/>
+            <Input className="input" type="text" name={'inputTo'} value={this.state.inputTo} onChange={this.handleChange} onClick={this.inputClick}/>
+            {/* <List className="list-country" country={this.state.country} onClick={this.listClick2}/> */}
           </div>
           <Button className="btn-convert" value={'Convert'} onClick = {this.handleClick}/>
         </div>
         <div className="">
           <h1 className="resultConvert" name="resultConvert">{this.state.resultConvert}</h1>
         </div>
+        <p className="note">Example = Amount : 1, From : USD or IDR etc, To : IDR or USD etc</p>
       </div>
     );
   }
